0e51f57557ae8020bfec3d924cc23c9729163f99
Jared Folkins <jfolkins@gmail.com>
2010-01-06 10:35:45 +0800

    Implemented hash value footprint, implemented spam-honeypot admin menu footprint
    Signed-off-by: Jared Folkins <jfolkins@gmail.com>

--- spam-honeypot.php
+++ spam-honeypot.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
 Plugin Name: Spam Honey Pot
 Plugin URI: http://wordpress.org/extend/plugins/spam-honeypot
@@ -8,16 +9,52 @@ Author: Matthew Turland
 Author URI: http://matthewturland.com
 */
 
+/*
+ADMIN
+*/
+
+if (is_admin()) {
+	add_action('admin_menu','menu_honeypot');
+	add_action('admin_init','init_honeypot');
+	register_activation_hook(__FILE__,'register_honeypot');
+}
+
+function menu_honeypot() {
+	add_options_page('Honeypot Settings','Spam-Honeypot','administrator','jared folkins','options_page_honeypot');
+}
+
+function init_honeypot() {
+	if (function_exists('register_setting')) {
+		register_setting('options-honeypot', 'hash_honeypot');
+	}
+}
+
+function options_page_honeypot() {
+	echo '<div class="wrap">';
+	echo '<p>Here is where the Honeypot options form will go.</p>';
+	echo '</div>';
+}
+
+function register_honeypot() {
+	add_option('hash_honeypot','default-hash');
+}
+
+/*
+MAIN
+*/
+
 add_action('comment_form', 'add_honeypot');
 add_filter('pre_comment_approved', 'check_honeypot');
 
 function add_honeypot($postID) {
-    echo '<textarea name="more_comment" style="display: none;"></textarea>';
+	$hash = get_option('hash_honeypot');
+	echo '<textarea name="'.$hash.'" style="display: none;"></textarea>';
 }
 
 function check_honeypot($approved) {
-    if (!empty($_POST['more_comment'])) {
-        $approved = 'spam';
-    }
-    return $approved;
+	$hash = get_option('hash_honeypot');
+	if (!empty($_POST[$hash])) {
+		$approved = 'spam';
+	}
+	return $approved;
 }
