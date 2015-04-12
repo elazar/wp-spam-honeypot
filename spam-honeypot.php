7247e4ce69ce98bf820d2491555fc921689098c7
Matthias-Christian Ott <ott@mirix.org>
2015-04-12 12:06:42 +0000

    Make indentation consistent
    The source code uses a mix of tabs and spaces that is inconsistent and
    makes it less maintainable. Making it consistent therefore makes it
    more maintainable.

--- spam-honeypot.php
+++ spam-honeypot.php
@@ -16,7 +16,7 @@ ADMIN
 if (is_admin()) {
 	add_action('admin_menu', 'menu_honeypot');
 	add_action('admin_init', 'init_honeypot');
-    add_filter('plugin_action_links', 'add_action_link_honeypot', 10, 2);
+	add_filter('plugin_action_links', 'add_action_link_honeypot', 10, 2);
 	register_activation_hook(__FILE__, 'register_honeypot');
 }
 
@@ -25,15 +25,15 @@ function menu_honeypot() {
 }
 
 function init_honeypot() {
-    register_setting('spam-honeypot', 'textarea_name');
-    register_setting('spam-honeypot', 'submit_name');
+	register_setting('spam-honeypot', 'textarea_name');
+	register_setting('spam-honeypot', 'submit_name');
 }
 
 function add_action_link_honeypot($links, $file) {
-    if ($file == basename(__FILE__)) {
-        array_unshift($links, '<a href="options-general.php?page=spam-honeypot">' . __('Settings','spam-honeypot') . '</a>');
-    }
-    return $links;
+	if ($file == basename(__FILE__)) {
+array_unshift($links, '<a href="options-general.php?page=spam-honeypot">' . __('Settings','spam-honeypot') . '</a>');
+	}
+	return $links;
 }
 
 function register_honeypot() {
@@ -78,21 +78,21 @@ add_filter('pre_comment_approved', 'check_honeypot');
 
 function add_honeypot($postID) {
 	load_plugin_textdomain( 'spam-honeypot', WP_PLUGIN_DIR .'/wp-spam-honeypot/languages', '/wp-spam-honeypot/languages' );
-    $textarea_name = get_option('textarea_name');
+	$textarea_name = get_option('textarea_name');
 	echo '<p style="display:none">';
 	echo '<textarea name="' . $textarea_name . '" cols="100%" rows="10"></textarea>';
-	echo '<label  for="' . $textarea_name . '">' . __('If you are a human, do not fill in this field.','spam-honeypot') . '</label>';	
+	echo '<label  for="' . $textarea_name . '">' . __('If you are a human, do not fill in this field.','spam-honeypot') . '</label>';
 	echo '</p>';
 	
 }
 
 function check_honeypot($approved) {
-    $textarea_name = get_option('textarea_name');
+	$textarea_name = get_option('textarea_name');
 	$submit_name = get_option('submit_name');
 	if (!empty($_POST[$textarea_name]) // Bot filled out the hidden textarea
-        || (!empty($submit_name) // User specified a value for the submit button
-        && empty($_POST[$submit_name]))) { // Bot didn't include a value for the submit button 
+	    || (!empty($submit_name) // User specified a value for the submit button
+	    && empty($_POST[$submit_name]))) { // Bot didn't include a value for the submit button 
 		$approved = 'spam';
-    }
+	}
 	return $approved;
 }
