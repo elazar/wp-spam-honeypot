00597e95f4d9e034c2b0a0c24e17f70263f46e96
Matthew Turland <tobias382@gmail.com>
2015-04-12 12:24:16 -0500

    Added license information to plugin source file

--- spam-honeypot.php
+++ spam-honeypot.php
@@ -7,6 +7,25 @@ Description: Adds a hidden text field to the comment form to trap spam bots.
 Version: 1.0.0
 Author: Matthew Turland
 Author URI: http://matthewturland.com
+License: GPL2
+*/
+
+/*
+Copyright 2010-2015 Matthew Turland (me@matthewturland.com)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as 
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
 */
 
 /*
